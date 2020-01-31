1b79bd5a76a452d51cc7825b840b20b51c4d0c95
Viktor Zhurbin <vzhurbin@gmail.com>
2020-01-31 15:46:38 +0300

    set up proxy

--- client/src/tools/api.ts
+++ client/src/tools/api.ts
@@ -1,5 +1,5 @@
 export const postResponse = async (requestBody = {}) => {
-    const encodedURI = window.encodeURI('http://localhost:4000/finch-test');
+    const encodedURI = window.encodeURI('/finch-test');
     const response = await fetch(encodedURI, {
         method: 'POST',
         body: JSON.stringify(requestBody),
--- server/app.js
+++ server/app.js
@@ -1,22 +1,18 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const app = express();
-
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-
-const port = 4000;
 
 app.post("/finch-test", (req, res) => {
     res.send({ success: true });
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+const port = 4000;
+const server = app.listen(port, () =>
+    console.log(`App ready on port ${port}!`)
+);
+
+process.on("SIGTERM", () => {
+    server.close(() => {
+        console.log("Process terminated");
+    });
+});
